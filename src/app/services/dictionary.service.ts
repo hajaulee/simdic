6461b2807a2a9dc100b5dc34f2f79d89348d4be1
hajaulee <anhlahau.hl@gmail.com>
2022-08-02 23:53:33 +0900

    build

--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {httpGetAsync} from "./http-utils";
-import {map} from "rxjs/operators";
+import {catchError, map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,29 +25,32 @@ export class DictionaryService {
   }
 
   search(searchKey: string): Observable<any> {
-    // return this.httpClient.post("https://api.mazii.net/api/search",
-    //   {
-    //     dict: "javi",
-    //     limit: 2,
-    //     page: 1,
-    //     query: searchKey,
-    //     type: "word"
-    //   }
-    // );
-
-    return httpGetAsync(
-      `https://vi.mazii.net/api/search?dict=javi&limit=2&page=1&query=${searchKey}&type=word`,
-      true,
-      true,
+    return this.httpClient.post("https://vi.mazii.net/api/search",
       {
-        minLength: 100,
-        useCache: false,
-        cacheTime: 1500,
-        method: "POST"
+        dict: "javi",
+        limit: 2,
+        page: 1,
+        query: searchKey,
+        type: "word"
       }
     ).pipe(
-      map((data) => JSON.parse(data))
-    )
+      catchError((err) => {
+        console.log(err);
+        return httpGetAsync(
+          `https://vi.mazii.net/api/search?dict=javi&limit=2&page=1&query=${searchKey}&type=word`,
+          true,
+          true,
+          {
+            minLength: 100,
+            useCache: false,
+            cacheTime: 1500,
+            method: "POST"
+          }
+        ).pipe(
+          map((data) => JSON.parse(data))
+        )
+      })
+    );
   }
 
   getComments(wordId: number, token: string): Observable<any> {
